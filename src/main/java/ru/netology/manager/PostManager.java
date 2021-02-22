d514227d46407652ddaa581dd7dee3246d5c1cec
Rasul Akhmetov <rasul.akhmetov@yandex.ru>
2021-02-23 00:13:24 +0300

    V2

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    public Post[] wallSearch(int ownerId, String domain, String query, int count, int offset) {
+        return null;
+    }
+
+    public void wallDelete( int ownerId, int postId){
+        return;
+    }
+}
